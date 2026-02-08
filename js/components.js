0bbd644545bf8b2a89e610af1c64781862798556
DETROMIN27 <94011680+DETROMIN27@users.noreply.github.com>
2026-02-08 12:26:56 +0200

    /

--- js/components.js
+++ js/components.js
@@ -3,3 +3,14 @@ async function loadComponent(name_file, name_sel){
     let div_cont = document.querySelector(name_sel)
     div_cont.innerHTML = await component.text()
 }
+async function loadData(name_file) {
+    let data = await fetch(name_file)
+    let data_obg = await data.json()
+    return data_obg
+}
+function choice(arr) {
+  if (!arr.length) {
+    throw new Error("Cannot choose from an empty array");
+  }
+  return arr[Math.floor(Math.random() * arr.length)];
+}
--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
     loadComponent('../components/header.html', '.header')
+
+    
+    let all_product = loadData('../data/products.json')
+    loadData('../data/products.json').then(function(data){
+        all_product = data
+    })
+    console.log(all_product)
 })
